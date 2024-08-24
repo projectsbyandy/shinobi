3e6c9b3e020f0b20f9e0e6912b99e1c607bbed16
Andrew Chang <mrchang@outlook.com>
2024-08-24 17:16:51 +0100

    Added doc comments to ninja service

--- src/Shinobi.FunctionApp/Services/IShinobiService.cs
+++ src/Shinobi.FunctionApp/Services/IShinobiService.cs
@@ -5,8 +5,25 @@ namespace Shinobi.FunctionApp.Services;
 
 public interface IShinobiService
 {
+    /// <summary>
+    /// Returns Ninja details
+    /// </summary>
+    /// <param name="id">Ninja id to retrieve</param>
     public ShinobiServiceResponse<Ninja> Get(int id);
+    
+    /// <summary>
+    /// <returns>
+    /// All Ninja details
+    /// </returns>
+    /// </summary>
     public ShinobiServiceResponse<IEnumerable<Ninja>> GetAll();
+    
+    /// <summary>
+    /// <returns>
+    /// Generated random Ninja
+    /// </returns>
+    /// <param name="numberOfNinjas">Number of Ninja to seed</param>
+    /// </summary>
     public ShinobiServiceResponse<IEnumerable<Ninja>> Seed(int numberOfNinjas);
 
 }
